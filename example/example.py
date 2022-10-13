8a1c46d37bf155865987e17897907d7e03e2d521
Ha Jong Kim <93378425+leftgoes@users.noreply.github.com>
2022-10-14 00:35:36 +0200

    Update example.py

--- example/example.py
+++ example/example.py
@@ -1,6 +1,6 @@
 from animate import MuseScore
 
 if __name__ == '__main__:
-  mscore = MuseScore(1/16, log_file='log.txt')
-    mscore.read_musicxml('chopin_etude_10_4.musicxml')
+  mscore = MuseScore(1/16)
+    mscore.read_musicxml('chopin_op10_no4.musicxml')
     mscore.get_frames('frames', single_page=True, dpi=187)
