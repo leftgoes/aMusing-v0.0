050fa7bead5648983fcfd34b3f89e21510589644
Ha Jong Kim <93378425+leftgoes@users.noreply.github.com>
2022-11-09 04:23:25 +0100

    Delete example directory

--- example/example.py
+++
@@ -1,6 +0,0 @@
-from animate import MuseScore
-
-if __name__ == '__main__:
-    mscore = MuseScore(1/16)
-    mscore.read_musicxml('chopin_op10_no4.musicxml')
-    mscore.get_frames('frames', single_page=True, dpi=187)
